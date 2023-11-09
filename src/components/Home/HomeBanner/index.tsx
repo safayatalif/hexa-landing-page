c51819ed6ca7f9dbf9d467ea3e2a9f84603ab3d8
safayatalif <safayatalif2021@gmail.com>
2023-11-09 18:35:15 +0600

    create 70% home page

--- src/components/Home/HomeBanner/index.tsx
+++ src/components/Home/HomeBanner/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 // import { HexaBetaLogo } from '../../assets';
-import { Typography } from '../../../common';
+import Quantum from '../../../assets/home/Quantum.png';
+import Velocity from '../../../assets/home/Velocity.png';
+import { Button, Typography } from '../../../common';
 import styles from './HomeBanner.module.scss';
 
 export const HomeBanner: React.FC = () => {
@@ -11,15 +13,42 @@ export const HomeBanner: React.FC = () => {
                     The most stylish learning <br /> experience. Ever.
                 </Typography>
                 <Typography variant="text-xs-regular">
-                    <p className={styles.p1}>
+                    <span className={styles.p1}>
                         Making high school exam preparations fun, delightful,
                         and cinematic.
-                    </p>
+                    </span>
                 </Typography>
             </div>
-            {/* <div>
-                    <img className={styles.laptopImage} src={laptop} alt="" />
-                </div> */}
+            <div className={styles.buttonContainer}>
+                <Button styleType="primary" className={styles.getButton}>
+                    <Typography variant="text-md-semibold" color="white">
+                        Get Started
+                    </Typography>
+                </Button>
+                <Button styleType="outline" className={styles.watchButton}>
+                    <Typography variant="text-md-semibold" color="white">
+                        Watch Physics trailer
+                    </Typography>
+                </Button>
+            </div>
+
+            <div className={styles.awardContainer}>
+                <img className={styles.award} src={Quantum} alt="" />
+                <img className={styles.award} src={Velocity} alt="" />
+            </div>
+            <div className={styles.bottomHeading}>
+                <Typography variant="title-2xl-semibold">
+                    <span className={styles.heading1}>Learn</span>{' '}
+                    <span className={styles.heading3}>and</span>{' '}
+                    <span className={styles.heading2}>
+                        Practice. <br />{' '}
+                    </span>{' '}
+                    <span className={styles.heading3}>
+                        Two birds with one <br />{' '}
+                    </span>{' '}
+                    <span className={styles.heading4}>stone.</span>
+                </Typography>
+            </div>
         </div>
     );
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { HomeBanner } from '../../components';
+import { HomeBanner, Paper } from '../../components';
+import CinematicVideo from '../../components/Home/CinematicVideo';
+import { Moving } from '../../components/Home/Moving';
 
 export const Home: React.FC = () => {
     return (
         <div>
             <HomeBanner></HomeBanner>
+            <CinematicVideo></CinematicVideo>
+            <Moving></Moving>
+            <Paper></Paper>
         </div>
     );
 };
